53d9c7e59f25d9e4c41ccaef29468e2e2d711a39
Ronan <ro.brett@gmail.com>
2014-07-11 16:26:18 +1000

    g

--- controllers/database.js
+++ controllers/database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql'),
     async = require('async');
 
 
-
+/*
 
 var client = mysql.createConnection({
     host: 'localhost',
@@ -15,8 +15,9 @@ var client = mysql.createConnection({
 });
 
 
+*/
 
-/*
+
 
 var client = mysql.createConnection({
     host: process.env.RDS_HOSTNAME,
@@ -26,7 +27,6 @@ var client = mysql.createConnection({
     database: 'mynode_db',
 
 })
-*/
 
 // Get records from a city
 exports.getRecords = function(city, callback) {
